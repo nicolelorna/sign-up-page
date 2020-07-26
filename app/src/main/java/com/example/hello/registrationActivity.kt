01d169fe4c4e00543f875a486f17354508ad441b
nicolelorna <nicolelornah02@gmail.com>
2020-07-27 02:55:48 +0300

    Fixed bug

--- app/src/main/java/com/example/hello/registrationActivity.kt
+++ app/src/main/java/com/example/hello/registrationActivity.kt
@@ -5,16 +5,16 @@ import android.os.Bundle
 
 class registrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
-        super onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnRegister.setOnClickListner{
-            var firstName=etFirstName.text.toString()
-            var lastName=etLastName.text.toString()
-            val email=etEmail.text.toString()
-            val phoneNumber=etPhoneNumber.text.toString()
-            val Password=etPassword.text.toString()
-            val passwordConfirmation=et.passwordConfirmation.toString( )
-        }
+//        btnRegister.setOnClickListner{
+//            var firstName=etFirstName.text.toString()
+//            var lastName=etLastName.text.toString()
+//            val email=etEmail.text.toString()
+//            val phoneNumber=etPhoneNumber.text.toString()
+//            val Password=etPassword.text.toString()
+//            val passwordConfirmation=et.passwordConfirmation.toString( )
+//        }
     }
 }
